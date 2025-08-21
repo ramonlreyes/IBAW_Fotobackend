896eb5e4d4350ab7d5eda2913ff287f1b91ee8f9
ramonlreyes <ramon.lorareyes@outlook.com>
2025-08-21 18:16:24 +0200

    changed the server cors acess url

--- backend/server.js
+++ backend/server.js
@@ -46,12 +46,7 @@ const staticFileLimiter = rateLimit({
 });
 
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? 'https://ramon-lora-reyes-photography-backend.onrender.com' 
-    : [ 'http://localhost:80',
-        'http://localhost:5173',
-        'http://localhost:3000'
-      ],
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 
